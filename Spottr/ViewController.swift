d3bfc2def5c4f26eaef91f7a80232263a3e291a9
Ginger Dudley <gingerdudley@gingers-macbook-pro.local>
2019-04-11 21:35:37 -0700

    Initial Commit

---
+++ Spottr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spottr
+//
+//  Created by Ginger Dudley on 4/11/19.
+//  Copyright © 2019 Ginger Dudley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
